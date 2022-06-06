5c2a723370ebac9daa6bff355a1012a9eb1794f6
jswob <jacswob@gmail.com>
2022-06-06 14:56:12 +0200

    feat: add flag indicating reachability

---
+++ app/components/online-status-sign.js
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class OnlineStatusSignComponent extends Component {
+  @tracked
+  isOnline = true;
+
+  updateIsOnline() {
+    this.isOnline = navigator.onLine;
+  }
+
+  @action
+  setup() {
+    ['load', 'online', 'offline'].forEach((eventName) =>
+      window.addEventListener(eventName, () => this.updateIsOnline())
+    );
+  }
+}
